0e271d0f6c7664ffa8bce81cb09144609ba7c01e
Marco Haber <marcoshaber99@gmail.com>
2025-02-09 11:22:54 +0200

    fix broken footer ui

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -7,12 +7,12 @@ export default function MarketingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="min-h-screen py-8">
+      <main className="flex-1 py-8">
         <div className="container mx-auto max-w-4xl px-4">{children}</div>
-        <Footer />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
